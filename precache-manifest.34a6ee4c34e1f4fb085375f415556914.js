2a5ba7240c1aecaab30c371f3004e2cc767c5527
mystardious <47038554+mystardious@users.noreply.github.com>
2020-05-30 03:16:37 +1000

    Updates

---
+++ precache-manifest.34a6ee4c34e1f4fb085375f415556914.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df985a7efd607da4a2f894c969877c03",
+    "url": "/boom-online/index.html"
+  },
+  {
+    "revision": "1d789cf017cae1f778b4",
+    "url": "/boom-online/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "64440514eb77091c4f46",
+    "url": "/boom-online/static/js/2.430ed831.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/boom-online/static/js/2.430ed831.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d789cf017cae1f778b4",
+    "url": "/boom-online/static/js/main.e4c73dc9.chunk.js"
+  },
+  {
+    "revision": "11383078dfe8db7bca67",
+    "url": "/boom-online/static/js/runtime-main.b1968fb3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/boom-online/static/media/logo.5d5d9eef.svg"
+  }
+]);
